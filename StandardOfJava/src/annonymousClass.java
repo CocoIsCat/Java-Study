826c8a0abdf8889d265d10ad0822e3796c77b24c
cocofarm <demy176@naver.com>
2022-07-09 15:13:02 +0900

    익명 클래스 예제

---
+++ StandardOfJava/src/annonymousClass.java
@@ -0,0 +1,9 @@
+public class annonymousClass {
+
+    Object iv = new Object() {  //익명 클래스
+        void method(){};
+    };
+    static Object cv = new Object() {   //익명 클래스
+        void method() {};
+    };
+}
